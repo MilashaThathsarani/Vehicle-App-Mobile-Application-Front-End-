3c349c1289c18849382f0446f5284ea6cab62549
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-08 14:10:05 +0530

    Updated Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -9,9 +9,9 @@ function Login() {
       <Text color={'#218c74'} alignItems='center' mt="20" fontSize="30" fontWeight={'bold'}>Welcome !</Text>
       <Input variant="outline" placeholder="User Name" borderColor={'#218c74'} mt='20' width='80%'/>
       <Input variant="outline" placeholder="Password" borderColor={'#218c74'} mt='8' width='80%'/>
-      <Button size="md" backgroundColor={'#218c74'} width='40%' mt='10'>Login</Button>
-      <Text color={'black'} alignItems='center' mt="20" fontSize="15">Don't have an account ?</Text>
-      <Link href="https://nativebase.io">Register</Link>
+      <Button size="md" backgroundColor={'#218c74'} width='40%' mt='10' >Login</Button>
+      <Text color={'#218c74'} alignItems='center' mt="20" fontSize="15">Don't have an account ?</Text>
+      <Link href="" color={'#218c74'}>Register Now !</Link>
       </VStack>
     </NativeBaseProvider>
   )
