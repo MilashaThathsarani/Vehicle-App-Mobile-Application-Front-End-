ab1136c6be0597a02d7ad35be58e166aa893bfc8
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-08 14:05:42 +0530

    Updated Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,18 @@
 
 import React from 'react'
-import { NativeBaseProvider, Text } from "native-base";
+import { NativeBaseProvider, Text,Input,VStack,Button,Link} from "native-base";
 
 function Login() {
   return (
     <NativeBaseProvider>
-      <Text>Login</Text>
+      <VStack space={3} alignItems="center">
+      <Text color={'#218c74'} alignItems='center' mt="20" fontSize="30" fontWeight={'bold'}>Welcome !</Text>
+      <Input variant="outline" placeholder="User Name" borderColor={'#218c74'} mt='20' width='80%'/>
+      <Input variant="outline" placeholder="Password" borderColor={'#218c74'} mt='8' width='80%'/>
+      <Button size="md" backgroundColor={'#218c74'} width='40%' mt='10'>Login</Button>
+      <Text color={'black'} alignItems='center' mt="20" fontSize="15">Don't have an account ?</Text>
+      <Link href="https://nativebase.io">Register</Link>
+      </VStack>
     </NativeBaseProvider>
   )
 }
