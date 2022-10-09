9cc0640c5b945e3f258f5a73f717e8774c1be546
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-09 17:27:29 +0530

    Updated Files

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, FlatList,TouchableOpacity,Text} from 'react-native'
+import React, { useEffect ,useState} from 'react'
 
-export default function Home() {
+export default function Home({navigation}) {
+
+  const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((json) => setPosts(json));
+    })
+
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <View style={{padding:20}}>
+      <Text>Vehicle Details</Text>
+    <FlatList
+        data={posts}
+        renderItem={({ item }) =>
+            <TouchableOpacity onPress={()=>navigation.navigate("ModifyCar",{obj:item})} style={{borderWidth:1, marginBottom:'5%', padding:5}}>
+                <Text style={{marginBottom:10,fontWeight:'bold'}} >{item.title}</Text>
+                <Text style={{marginBottom:10}} >{item.body}</Text>
+            </TouchableOpacity>
+        }
+    />
+</View>
   )
 }
