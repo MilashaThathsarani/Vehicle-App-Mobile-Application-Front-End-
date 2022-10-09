dd8eb92419ff043e517a363f011059366379eebf
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-09 08:30:43 +0530

    Updated

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Register from './screens/Register'
 export default function App() {
   return (
  //<FirstPage/>
- //<Login/>
-<Register/>
+ <Login/>
+//<Register/>
   )
 }
--- screens/Login.js
+++ screens/Login.js
@@ -7,9 +7,9 @@ function Login() {
     <NativeBaseProvider>
       <VStack space={3} alignItems="center">
       <Text color={'#182C61'} alignItems='center' mt="20" fontSize="30" fontWeight={'bold'}>Welcome !</Text>
-      <Input variant="outline" placeholder="User Name" borderColor={'#182C61'} mt='16' width='80%'/>
-      <Input variant="outline" placeholder="Password" borderColor={'#182C61'} mt='4' width='80%'/>
-      <Button size="md" backgroundColor={'#182C61'} width='40%' mt='7' borderRadius='20'>Login</Button>
+      <Input variant="outline" placeholder="User Name" borderColor={'#182C61'} mt='16' width='80%'borderRadius='20'/>
+      <Input variant="outline" placeholder="Password" borderColor={'#182C61'} mt='3' width='80%' borderRadius='20'/>
+      <Button size="md" backgroundColor={'#182C61'} width='40%' mt='8' borderRadius='20'>Login</Button>
       <Text color={'#182C61'} alignItems='center' mt="39" fontSize="15">Don't have an account ?</Text>
       <Link href="" color={'#182C61'}>Register Now !</Link>
       <Image size={10} borderRadius={80} source={require('../assets/car.png')} alt="Alternate Text" mt='71' />
