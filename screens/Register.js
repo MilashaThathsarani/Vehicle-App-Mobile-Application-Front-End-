8f1ba2246bef3df97a8157bbf770c828344de6d1
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-09 08:28:45 +0530

    Updated

--- screens/Register.js
+++ screens/Register.js
@@ -6,10 +6,10 @@ function Register() {
     <NativeBaseProvider>
     <VStack space={3} alignItems="center">
     <Text color={'#182C61'} alignItems='center' mt="20" fontSize="30" fontWeight={'bold'}>Register</Text>
-    <Input variant="outline" placeholder="User Name" borderColor={'#182C61'} mt='16' width='80%'/>
-    <Input variant="outline" placeholder="Email" borderColor={'#182C61'} mt='4' width='80%'/>
-    <Input variant="outline" placeholder="Password" borderColor={'#182C61'} mt='4' width='80%'/>
-    <Button size="md" backgroundColor={'#182C61'} width='40%' mt='7'>Save</Button>
+    <Input variant="outline" placeholder="User Name" borderColor={'#182C61'} mt='16' width='80%' borderRadius='20'/>
+    <Input variant="outline" placeholder="Email" borderColor={'#182C61'} mt='3' width='80%' borderRadius='20'/>
+    <Input variant="outline" placeholder="Password" borderColor={'#182C61'} mt='3' width='80%' borderRadius='20'/>
+    <Button size="md" backgroundColor={'#182C61'} width='40%' mt='7' borderRadius='22'>Create Account</Button>
     </VStack>
   </NativeBaseProvider>
   )
