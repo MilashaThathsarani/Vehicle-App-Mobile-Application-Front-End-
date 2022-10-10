e239f517a99a269c1e98089b4e0e855fabf1d295
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-10-10 21:57:16 +0530

    Updated files

--- screens/Login.js
+++ screens/Login.js
@@ -5,24 +5,38 @@ import { Alert } from 'react-native';
 
 function Login({navigation}) {
 
-  const[email,setEmail]=useState('');
-    const[password,setPassword]=useState('');
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
+  loginUser = () => {
+    if (userName != "" && password != "") {
+      fetch(`http://192.168.8.109:8000/users/login/${userName}/${password}`, {
+        method: "GET",
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          if (json.length === 0) {
+            Alert.alert("Username or password incorrect.Try again!")
+          } else {
+            clearTextFields()
+            Alert.alert("Login Successful.");
+            navigation.navigate("Home")
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      Alert.alert("Please Enter Username and Password and try again.")
+    }
+  }
 
-  const saveLogin=()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    email:email,
-    password:password
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-.then((response) => {Alert.alert("Login Successfully !")})
-.catch((err)=>{Alert.alert("Error occured !")})
-}
+  clearTextFields = () => {
+    setUserName("");
+    setPassword("");
+  }
 
   return (
     <NativeBaseProvider>
@@ -32,7 +46,7 @@ function Login({navigation}) {
       <Input value={password} onChangeText={(e)=>{setPassword(e)}} variant="outline" placeholder="Password" borderColor={'#182C61'} mt='3' width='80%' borderRadius='20'/>
       <Button size="md" backgroundColor={'#182C61'} width='40%' mt='8' borderRadius='20'onPress={saveLogin}>Login</Button>
       <Text color={'#182C61'} alignItems='center' mt="39" fontSize="15">Don't have an account ?</Text>
-      <Link href="" color={'#182C61'} onPress={()=>{navigation.navigate("Register")}}>Register Now !</Link>
+      <Link href="" color={'#182C61'} onPress={() => { loginUser()}}>Register Now !</Link>
       <Image size={10} borderRadius={80} source={require('../assets/car.png')} alt="Alternate Text" mt='71' />
     
       </VStack>
